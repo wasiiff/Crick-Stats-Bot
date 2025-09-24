e40ba44095d536de9be2571ce5cd3bf65266479a
wasiiff <wasifgee0012@gmail.com>
2025-09-24 11:37:38 +0500

    All Done

---
+++ client/app/@types/types.ts
@@ -0,0 +1,20 @@
+export type Role = "user" | "assistant";
+
+export interface MessagePayloadText {
+  type: "text";
+  text: string;
+}
+
+export interface MessagePayloadTable {
+  type: "table";
+  text?: string;
+  columns: string[];
+  rows: unknown[][];
+}
+
+export type MessagePayload = string | MessagePayloadText | MessagePayloadTable;
+
+export interface Message {
+  role: Role;
+  payload: MessagePayload;
+}
